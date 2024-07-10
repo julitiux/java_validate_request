51ee9504d584923cb113de006118b42d8c3b7ead
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-07-09 19:19:59 -0600

    starting repository java_validate_request

---
+++ src/main/java/com/java_validate_request/JavaValidateRequestApplication.java
@@ -0,0 +1,13 @@
+package com.java_validate_request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaValidateRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaValidateRequestApplication.class, args);
+	}
+
+}
