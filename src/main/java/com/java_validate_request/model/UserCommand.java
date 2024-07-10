a5bd6844558d9c1e9371be7e62ead47d76a23f46
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-07-10 15:04:49 -0600

    implement first instance of User command, just username and email

--- src/main/java/com/java_validate_request/model/UserCommand.java
+++ src/main/java/com/java_validate_request/model/UserCommand.java
@@ -1,4 +1,21 @@
 package com.java_validate_request.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class UserCommand {
+
+  @Size(max = 15, message = "should be max 15 characters")
+  @NotNull(message = "the field cannot be null")
+  @NotBlank(message = "the field cannot be empty")
+  private String username;
+
+  @Email(message = "invalid email")
+  @NotNull(message = "the field cannot be null")
+  private String email;
+
 }
