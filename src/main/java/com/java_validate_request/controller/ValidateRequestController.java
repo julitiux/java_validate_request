438c8ed47832488d1e3a7973c84bfa700cf4ddb7
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-07-10 19:28:49 -0600

    add tag @ResponseStatus just for the endpoint response with a 204 with a No Content

--- src/main/java/com/java_validate_request/controller/ValidateRequestController.java
+++ src/main/java/com/java_validate_request/controller/ValidateRequestController.java
@@ -3,11 +3,9 @@ package com.java_validate_request.controller;
 import com.java_validate_request.model.UserCommand;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Validated
@@ -16,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ValidateRequestController {
 
   @PostMapping("/post")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   public void post(@Valid @RequestBody UserCommand command) {
     log.info("{}", command.getUsername());
     log.info("{}", command.getEmail());
