03c98ee82ecb16eab0ca9ce9c627e3e332612c6f
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-07-10 19:22:17 -0600

    implement a simple ValidateRequest controller

--- src/main/java/com/java_validate_request/controller/ValidateRequestController.java
+++ src/main/java/com/java_validate_request/controller/ValidateRequestController.java
@@ -1,5 +1,21 @@
 package com.java_validate_request.controller;
 
+import com.java_validate_request.model.UserCommand;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RestController
+@RequestMapping("/api")
 public class ValidateRequestController {
+
+  @PostMapping("/post")
+  public void post(@RequestBody UserCommand command) {
+    log.info("{}", command.getUsername());
+    log.info("{}", command.getEmail());
+  }
+
 }
