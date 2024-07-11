2b87ae5348fce4b397c4f2beb7e484c49fe14358
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-07-10 19:23:30 -0600

    add tag @Validated just for active the validation

--- src/main/java/com/java_validate_request/controller/ValidateRequestController.java
+++ src/main/java/com/java_validate_request/controller/ValidateRequestController.java
@@ -2,12 +2,14 @@ package com.java_validate_request.controller;
 
 import com.java_validate_request.model.UserCommand;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/api")
 public class ValidateRequestController {
